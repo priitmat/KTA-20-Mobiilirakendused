9655df5a9c6e3ef71b0f50ceef54d121361a6ff1
Priit Matttus <priit.mattus@tptlive.ee>
2021-10-16 12:32:13 +0300

    Added calculator sample code

--- FirstApp/FirstApp/MainActivity.cs
+++ FirstApp/FirstApp/MainActivity.cs
@@ -1,20 +1,43 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace FirstApp
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", Theme = "@style/MyTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView firstText;
+        TextView answerTextView;
+        EditText firstNumberEditText;
+        EditText secondNumberEditText;
+        Button addButton;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            
+            firstNumberEditText = FindViewById<EditText>(Resource.Id.editText1);
+            secondNumberEditText = FindViewById<EditText>(Resource.Id.editText2);
+            addButton = FindViewById<Button>(Resource.Id.addButton);
+            answerTextView = FindViewById<TextView>(Resource.Id.answerTextView);
+            addButton.Click += AddButton_Click;
         }
+
+        private void AddButton_Click(object sender, System.EventArgs e)
+        {
+            var firstNumber = double.Parse(firstNumberEditText.Text);
+            var secondNumber = double.Parse(secondNumberEditText.Text);
+            var answer = firstNumber + secondNumber;
+
+            answerTextView.Text = answer.ToString();
+        }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
