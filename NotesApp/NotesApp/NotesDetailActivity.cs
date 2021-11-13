aadf8646d1e87d21e0f113fb2e8050851fc77e72
Priit Matttus <priit.mattus@tptlive.ee>
2021-11-13 09:54:38 +0200

    NotesApp Start

---
+++ NotesApp/NotesApp/NotesDetailActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NotesApp
+{
+    [Activity(Label = "NotesDetailActivity")]
+    public class NotesDetailActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.notes_detail_layout);
+        }
+    }
+}
