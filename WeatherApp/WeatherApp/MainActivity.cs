3f2365651defb17f61672f321331f8bbdfd9cc21
Priit Matttus <priit.mattus@tptlive.ee>
2021-11-27 12:01:02 +0200

    picture download

--- WeatherApp/WeatherApp/MainActivity.cs
+++ WeatherApp/WeatherApp/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 using WeatherApp.Services;
 
@@ -15,9 +17,24 @@ namespace WeatherApp
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            var cityEditText = FindViewById<EditText>(Resource.Id.cityEditText);
+            var searchButton = FindViewById<Button>(Resource.Id.searchButton);
+            var temperatureTextView = FindViewById<TextView>(Resource.Id.TemperatureTextView);
+            var windTextView = FindViewById<TextView>(Resource.Id.WindTextView);
+            var weatherImageView = FindViewById<ImageView>(Resource.Id.weatherImageView);
 
             var weatherService = new WeatherService();
-            var info = weatherService.GetCityWeather();
+
+            searchButton.Click += async delegate
+            {
+                var data = await weatherService.GetCityWeather(cityEditText.Text);
+                temperatureTextView.Text = data.main.temp.ToString();
+                windTextView.Text = data.wind.speed.ToString();
+
+                var imageBytes = await weatherService.GetImageFromUrl($"https://openweathermap.org/img/wn/{data.weather[0].icon}@2x.png");
+                var bitmap = await BitmapFactory.DecodeByteArrayAsync(imageBytes, 0, imageBytes.Length);
+                weatherImageView.SetImageBitmap(bitmap);
+            };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
--- WeatherApp/WeatherApp/Services/WeatherService.cs
+++ WeatherApp/WeatherApp/Services/WeatherService.cs
@@ -19,23 +19,36 @@ namespace WeatherApp.Services
     {
         const string ApiKey = "a519d2565f58343b5f157d056e658aca"; 
 
-        public async Task<WeatherInfo> GetCityWeather()
+        public async Task<WeatherInfo> GetCityWeather(string city)
         {
             var client = new HttpClient();
 
             try
             {
                 var response = await client.GetStringAsync
-                ($"https://api.openweathermap.org/data/2.5/weather?q=tallinn&units=metric&appid={ApiKey}");
+                ($"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={ApiKey}");
                 var data = JsonConvert.DeserializeObject<WeatherInfo>(response);
                 return data;
             }
             catch (Exception ex)
             {
-
                 throw ex;
             }
             
+        }
+
+        public async Task<Byte[]> GetImageFromUrl(string url)
+        {
+            using (var client = new HttpClient()) 
+            {
+                var msg = await client.GetAsync(url);
+                if (msg.IsSuccessStatusCode)
+                {
+                    var byteArray = await msg.Content.ReadAsByteArrayAsync();
+                    return byteArray;
+                }
+                return null;
+            }
         }
     }
 }
